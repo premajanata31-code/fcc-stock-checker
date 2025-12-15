1789357a990b54008d9ba42c71a486353a2878dd
premajanata31-code <wayandeta31@gmail.com>
2025-12-15 09:25:15 +0800

    2_functional-tests.js
    updated set from default

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,4 +7,66 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
+  test('Viewing one stock: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'GOOG' })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.property(res.body.stockData, 'stock');
+        assert.property(res.body.stockData, 'price');
+        assert.property(res.body.stockData, 'likes');
+        done();
+      });
+  });
+
+  test('Viewing one stock and liking it: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'MSFT', like: 'true' })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.equal(res.body.stockData.stock, 'MSFT');
+        assert.isAtLeast(res.body.stockData.likes, 1);
+        done();
+      });
+  });
+
+  test('Viewing the same stock and liking it again: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'MSFT', like: 'true' })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        // Kita tidak bisa assert jumlah pasti likes karena shared DB, tapi bisa cek structure
+        assert.equal(res.body.stockData.stock, 'MSFT');
+        assert.property(res.body.stockData, 'likes');
+        done();
+      });
+  });
+
+  test('Viewing two stocks: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'MSFT'] })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body.stockData);
+        assert.property(res.body.stockData[0], 'rel_likes');
+        done();
+      });
+  });
+
+  test('Viewing two stocks and liking them: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'MSFT'], like: 'true' })
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body.stockData);
+        assert.property(res.body.stockData[0], 'rel_likes');
+        done();
+      });
+  });
+
 });
